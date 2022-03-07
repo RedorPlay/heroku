c436202d690f5898349238d1835905c9c304eeac
Фёдор <klim.fed8400@mail.ru>
2022-03-07 13:17:31 +0400

    abc12

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Привет от приложения Flask и Феди"
+    return "Привет от приложения Flask ии Феди"
 
 
 if __name__ == '__main__':
